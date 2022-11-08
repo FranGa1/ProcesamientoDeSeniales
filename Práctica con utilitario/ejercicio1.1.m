7cd2ecf96e9f6ad949f16462a0b55426bf1d59c8
fran <franciscogaray@live.com.ar>
2022-11-08 14:33:44 -0300

    m

--- Práctica con utilitario/ejercicio1.1.m
+++ Práctica con utilitario/ejercicio1.1.m
@@ -1,12 +1,17 @@
-addpath("../AnSyS2022_PU1_m")
+addpath("./AnSyS2022_PU1_m")
 
+# Generación y gráfico de la señal
 [n, x] = senial(26287);
 plotCompletoSVID(n,x, 'n', 'x[n]', 'Señal ejercicio 1');
-ds = 0.0001; s = [-1/2:ds:1/2]; X = zeros(size(s));
 
+# Cálculo de su TFTD
+ds = 0.0001; s = [-1/2:ds:1/2];
+X = zeros(size(s));
 for k = 1:length(s)
   X(k)=sum(x.*exp(-1i*2*pi*s(k)*n));
 end
 
-plotCompleto(s, abs(X), 's', 'Modulo de la TFTD{x[n]}');
+# Gráfico del módulo de la TFTD de x
+plotCompleto(s, abs(X), 's', 'Mod ulo de la TFTD{x[n]}');
+# Gráfico de la fase de la TFTD de x
 plotCompleto(s, angle(X), 's', 'Fase de la TFTD{x[n]}');
