b1a6197afeb1d0ac98050c957ec9d2a035a080ac
franga <franciscogaray@live.com.ar>
2022-11-10 16:18:58 -0300

    filtros

--- Práctica con utilitario/ejercicio_2_4.m
+++ Práctica con utilitario/ejercicio_2_4.m
@@ -1,8 +1,8 @@
 n = 0:53000;
 x = deltaK(n);
 y1 = sistema_ej_2(x);
-y_filtro1 = filtro_una_rama(y1);
+y_filtro2 = filtro_dos_ramas(y1);
 y_filtro3 = filtro_tres_ramas(y1); 
 
-plotCompletoSVID(n,y_filtro1, ' ', 'n', 'hf1[n]');
+plotCompletoSVID(n,y_filtro2, ' ', 'n', 'hf1[n]');
 plotCompletoSVID(n,y_filtro3, ' ', 'n', 'hf3[n]');
